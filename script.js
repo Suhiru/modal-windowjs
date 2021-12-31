6d47c3ba0681e2ec8751985e89ae0323253d3b3a
Suhiru Benjamin De Silva <66971839+Suhiru@users.noreply.github.com>
2021-12-31 19:27:40 +0100

    some js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModel = document.querySelectorAll('.show-modal');
+
+console.log(modal);
+console.log(overlay);
+console.log(btnCloseModal);
+console.log(btnOpenModel);
+
+for (let i = 0; i < btnOpenModel.length; i++) {
+  console.log(btnOpenModel[i].textContent);
+}
